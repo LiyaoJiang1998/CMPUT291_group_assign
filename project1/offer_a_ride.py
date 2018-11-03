3e3a399b6f20bde45a0c74b554cc210e2c16cc93
Zhijie(Max) Shen <zjshen@ualberta.ca>
2018-11-02 20:24:00 -0600

    offer_a_ride and findLocation working in progress

--- project1/offer_a_ride.py
+++ project1/offer_a_ride.py
@@ -1,25 +1,44 @@
 import sqlite3
 import re
+import findLocation
 
 conn = None
 cur = None
 result = None
+driver = None
+
 #get source location
 def getSource():
+    findLocation.findLocation('key')
+    return
 
 #get destination
 def getDestination():
 
+    return
+
+#get car number
+def getCarNo():
+
+    return
+
 #get enroute
 def getEnroute():
+
+    return
 
 #find location
 
 
 #checks if user input is valid (excluding locations)
 def checkValid(result):
+    #make sure length is 4
+    if (len(result) != 4):
+        print('invalid number of arguments')
+        return False
+
     #match date, format 'YYYY-MM-DD'
-    if (re.match('[1-9][0-9]{3}-(0[1-9]|1[0-2])-[0-9]{2}', result[0 == None])):
+    if (re.match('[1-9][0-9]{3}-(0[1-9]|1[0-2])-[0-9]{2}', result[0]) == None):
         print('invalid date')
         return False
 
@@ -37,15 +56,17 @@ def checkValid(result):
     if (re.match('[a-zA-Z]{1,10}', result[3]) == None):
         print('invalid luggage')
         return False
+    
+    return True
 
     
 
 #get user input
 def getOffer():
     #prompt user input
-        print('Input ride offer in the following format')
-        print('(quit) to exit offer posting')
-        print("(date, number of seats offered, price per seat, luggage description)")
+    print('Input ride offer in the following format')
+    print('(quit) to exit offer posting')
+    print("(date, number of seats offered, price per seat, luggage description)")
         
     while(1):
         info = input('ride offer: ')
@@ -63,8 +84,12 @@ def getOffer():
         print('input destination location keyword')
         getDestination()
 
+        #get carnumber
+        print('input car number(optional)')
+        getCarNo()
+
         #get enroute
-        print('input enroute locations')
+        print('input enroute locations(optional)')
         getEnroute()
         
         
@@ -74,8 +99,9 @@ def main():
     global conn, cur, result
     path = "./test.db"
     conn = sqlite3.connect(path)
-    cur = connection.cursor()
+    cur = conn.cursor()
     getOffer()
+    conn.close()
 
 
 if __name__ == "__main__":
