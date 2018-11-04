22af581687174e0e17a74b74dbd712673d1c8275
Liyao Jiang <liyao1@ualberta.ca>
2018-11-03 20:47:59 -0600

    working on the main function

--- project1/291project1.py
+++ project1/291project1.py
@@ -1,17 +1,51 @@
+import sys
 import sqlite3
 import login
 import rideRequest
-#import feature3
+import offer_a_ride
+# import searchRides
+import feature3
+import feature4
 
 def main():
-    conn = sqlite3.connect('./pro1.db')
-    email = login.login(conn)
-    while True:
-        print('''Select your operation:''')
-        op = input("1 Offer a ride.\n2 Search for rides.\n3 Book members or cancel bookings.\n4 Post ride requests.\n5 Search and delete ride requests.\n6 Logout")
+    dbName = sys.argv[1]
+    conn = sqlite3.connect('./%s'%(dbName))
+    # email = login.login(conn)
 
-    rideRequest.searchAndDelete(conn, email)
-    #feature3.listAllBookings(conn, email)
+    while True:
+        print('''Select your operation: ''')
+        op = input("1 Offer a ride.\
+                    \n2 Search for rides.\
+                    \n3 Book members or cancel bookings.\
+                    \n4 Post ride requests.\
+                    \n5 Search and delete ride requests.\
+                    \n6 Logout\n")
+
+        if op == '1':
+            pass
+        elif op == '2':
+            pass
+        elif op == '3':
+            pass
+        elif op == '4':
+            pass
+        elif op == '5':
+            pass
+        elif op == '6':
+            pass
+        else:
+            print("invalid input!, please try again")
+            continue
+
+    # rides = searchRides.searchRide(conn, email)
+
+
+    # while True:
+    #     print('''Select your operation:''')
+    #     op = input("1 Offer a ride.\n2 Search for rides.\n3 Book members or cancel bookings.\n4 Post ride requests.\n5 Search and delete ride requests.\n6 Logout")
+    #
+    # rideRequest.searchAndDelete(conn, email)
+    # #feature3.listAllBookings(conn, email)
 
 if __name__ == "__main__":
     main()
--- project1/login.py
+++ project1/login.py
@@ -5,6 +5,7 @@ import getpass
 user = ""
 
 def login(conn):
+    global user
     c = conn.cursor()
     print("Welcome to our system! Please login first.")
     # log in or create new account
@@ -35,67 +36,123 @@ def log(conn):
         if re.match("^\\(quit\\)$", userName):
             return False
         password = getpass.getpass("Please enter your password:")
-        # check if user name and password is valid
-        if re.match("^.+\\@(\\[?)[a-zA-Z0-9\\-\\.]+\\.([a-zA-Z]{2,3}|[0-9]{1,3})(\\]?)$", userName) and re.match("^[A-Za-z0-9_]*$", password):
-            # check if account exists
-            c.execute('SELECT * FROM members WHERE email=? and pwd=?;' , (userName, password))
-            row = c.fetchone()
-            if row is None:
-                print("User name does not exist or password is incorrect. Please try again")
-            # if exists
-            else:
-                print("Account " + userName + " is logged in")
-                user = userName
-                # print the messages
-                c.execute('SELECT msgTimestamp, sender, content, rno from inbox where email=? and seen=?;', (userName, 'n'))
-                messages = c.fetchall()
-                tmp = 1
-                for message in messages:
-                    print("Message" + str(tmp) + ":")
-                    print("time: " + message[0] + " sender " + message[1] + " content: " + message[2] + " rno " + str(message[3]))
-                    c.execute('UPDATE inbox set seen=? where msgTimestamp=? and email=?;', ('y', message[0], email))
-                    conn.commit()
-                    tmp += 1
-                return True
+        check = checkUserNameAndPassword(conn, userName, password)
+        if check is False:
+            continue
         else:
-            print("Invalid user name or password, pleae try again")
+            user = check
+            return True
+
+def checkUserNameAndPassword(conn, userName, password):
+    c = conn.cursor()
+    # check if user name and password is valid
+    if re.match("^.+\\@(\\[?)[a-zA-Z0-9\\-\\.]+\\.([a-zA-Z]{2,3}|[0-9]{1,3})(\\]?)$", userName) and re.match("^[A-Za-z0-9_]*$", password):
+        # check if account exists
+        c.execute('SELECT * FROM members WHERE email=? and pwd=?;' , (userName, password))
+        row = c.fetchone()
+        if row is None:
+            print("User name does not exist or password is incorrect. Please try again")
+            return False
+        # if exists
+        else:
+            print("Account " + userName + " is logged in")
+            # print the messages
+            showUnreadMessage(conn, userName)
+            return userName
+    else:
+        print("Invalid user name or password, pleae try again")
+        return False
+
+def showUnreadMessage(conn, userName):
+    c = conn.cursor()
+    c.execute('SELECT msgTimestamp, sender, content, rno from inbox where email=? and seen=?;', (userName, 'n'))
+    messages = c.fetchall()
+    tmp = 1
+    for message in messages:
+        print("Message" + str(tmp) + ":")
+        print("time: " + message[0] + " sender " + message[1] + " content: " + message[2] + " rno " + str(message[3]))
+        c.execute('UPDATE inbox set seen=? where msgTimestamp=? and email=?;', ('y', message[0], userName))
+        conn.commit()
+        tmp += 1
 
 def signup(conn):
     global user
     c = conn.cursor()
     while True:
-        # check the valid user name
-        print("if you want to go back to the last screen, enter (quit)")
-        userName = input("Please enter your user name (email): ")
-        if re.match("^\\(quit\\)$", userName):
+        userName = signupUserName(conn)
+        if userName is True:
             return False
-        if re.match("^.+\\@(\\[?)[a-zA-Z0-9\\-\\.]+\\.([a-zA-Z]{2,3}|[0-9]{1,3})(\\]?)$", userName):
-            # check if the user name already exists
-            c.execute('SELECT email FROM members WHERE email=?;' , (userName,))
-            row = c.fetchone()
-            if row is None:
-                break
-            else:
-                print("User name already exists. Please use another email.")
+        elif userName is False:
+            continue
         else:
-            print("invalid user name, please enter a valid email address")
-    while True:
-        # check valid password
-        password = getpass.getpass("Please enter your password:")
-        if re.match("^[A-Za-z0-9_]*$", password):
             break
-        else:
-            print("Invalid password format, please only use letter, number and undersccore")
-    # enter name and phone number
-    name = input("Please enter your name: ")
     while True:
-        phone = input("Please enter your phone number: ")
-        if re.match("^[0-9]*$", phone):
-            break
+        password = signupPwd(conn)
+        if password is False:
+            continue
         else:
-            print("Invalid phone number.")
+            break
+    while True:
+        name = signupName(conn)
+        if name is False:
+            continue
+        else:
+            break
+    while True:
+        phone = signupPhone(conn)
+        if phone is False:
+            continue
+        else:
+            break
     c.execute('INSERT into members values(?, ?, ?, ?);', (userName, name, phone, password))
     conn.commit()
     user = userName
     print("Account created!")
     return True
+
+def signupUserName(conn):
+    c = conn.cursor()
+    # check the valid user name
+    print("if you want to go back to the last screen, enter (quit)")
+    userName = input("Please enter your user name (email): ")
+    if re.match("^\\(quit\\)$", userName):
+        return True
+    if re.match("^.+\\@(\\[?)[a-zA-Z0-9\\-\\.]+\\.([a-zA-Z]{2,3}|[0-9]{1,3})(\\]?)$", userName):
+        # check if the user name already exists
+        c.execute('SELECT email FROM members WHERE email=?;' , (userName,))
+        row = c.fetchone()
+        if row is None:
+            return userName
+        else:
+            print("User name already exists. Please use another email.")
+    else:
+        print("invalid user name, please enter a valid email address")
+    return False
+
+def signupPwd(conn):
+    c = conn.cursor()
+    # check valid password
+    password = getpass.getpass("Please enter your password:")
+    if re.match("^[A-Za-z0-9_]*$", password):
+        return password
+    else:
+        print("Invalid password format, please only use letter, number and undersccore")
+        return False
+
+def signupName(conn):
+    c = conn.cursor()
+    name = input("Please enter your name: ")
+    if re.match("^[ A-Za-z]*$", name):
+        return name
+    else:
+        print("Invalid name style")
+        return False
+
+def signupPhone(conn):
+    c = conn.cursor()
+    phone = input("Please enter your phone number: ")
+    if re.match("^[0-9]*$", phone):
+        return phone
+    else:
+        print("Invalid phone number.")
+        return False
