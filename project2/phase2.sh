e81307cc9ea08863b05378123a058ce5804e43d4
LiyaoJiang1998 <liyao1@ualberta.com>
2018-11-25 12:04:55 -0700

    run by using sh -e phase2.sh

--- project2/phase2.sh
+++ project2/phase2.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
+# remove old txt files if exists
+rm -f ./outputs/*.txt
 # parse
 python3 phase1.py
 # remove old index files if exists
-rm -f *.idx
-
+rm -f ./outputs/*.idx
 # sort the .txt files, break the input, use given script break.pl to build .idx
 sort --unique ./outputs/ads.txt | perl break.pl | db_load -c duplicates=1 -T -t hash ./outputs/ad.idx
 sort --unique ./outputs/pdates.txt | perl break.pl | db_load -c duplicates=1 -T -t btree ./outputs/da.idx
 sort --unique ./outputs/terms.txt | perl break.pl | db_load -c duplicates=1 -T -t btree ./outputs/te.idx
 sort --unique ./outputs/prices.txt | perl break.pl | db_load -c duplicates=1 -T -t btree ./outputs/pr.idx
+# get query inputs
+python3 getQueryInput.py
